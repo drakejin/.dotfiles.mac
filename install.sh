9c8efddc6069b4b50d887633543884100e0f85a6
YongJin,Cho <dydwls121200@gmail.com>
2019-02-28 17:35:32 +0900

    tmux 사용안함

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ sh ~/.vim_runtime/install_basic_vimrc.sh
 echo "nodejs 버전관리 프로그램인 nvm을 설치합니다."
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-# zshrcCustom의 셋업을 불러옵니다.
+# zshrcCustom의 셋업을 불러옵니다
 echo "source ~/.dotfiles.mac/zshrcCustom" >> ~/.zshrc
 source ~/.zshrc
 
