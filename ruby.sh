a8ef711d4cfd29c6cc4f8cb7b84d85bb1b4dff38
YongJin,Cho <dydwls121200@gmail.com>
2019-04-25 18:25:13 +0900

    direnv 설정에 오타 존재

--- ruby.sh
+++ ruby.sh
@@ -1,6 +1,6 @@
 brew install direnv
 brew install ruby-install
 
-echo 'eval "$(direnv hook bash)"' >> ~/.zshrc
+echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
 
 ln -s ~/.dotfiles.mac/direnvrc ~/.direnvrc
